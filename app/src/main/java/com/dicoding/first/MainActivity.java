c2a35b6a097dfcca15b3fc63ec0fb1f6b131b19a
Evalina <evalinasimangunsong.es@gmail.com>
2018-12-03 20:51:31 +0700

    click button to another activity

--- app/src/main/java/com/dicoding/first/MainActivity.java
+++ app/src/main/java/com/dicoding/first/MainActivity.java
@@ -1,15 +1,27 @@
 package com.dicoding.first;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("test");
+        //inisialisasi button sesuai id yd ada di activity_main
+        button =(Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this, pindah.class);
+                startActivity(in);
+            }
+        });
 
     }
 
--- app/src/main/java/com/dicoding/first/pindah.java
+++ app/src/main/java/com/dicoding/first/pindah.java
@@ -1,9 +1,18 @@
 package com.dicoding.first;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Lenovo on 12/3/2018.
  */
-public class pindah {
+public class pindah extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pindah);
+
+    }
 
 }
